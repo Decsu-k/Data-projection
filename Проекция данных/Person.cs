26aa5414eb718958ebcdd4479bf0e16d2ab9baf2
k-roz <k-roz@DESKTOP-BMAD82K>
2022-02-14 09:14:19 +0300

    Добавьте файлы проекта.

---
+++ Проекция данных/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Проекция_данных
+{
+    internal class Person
+    {
+        private string v1;
+        private int v2;
+
+        public Person(string v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public object Name { get; internal set; }
+    }
+}
